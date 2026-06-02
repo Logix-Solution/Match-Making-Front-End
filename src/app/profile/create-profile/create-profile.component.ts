a256c20009378f2410c3d9204d90a5f8034113f5
haroonqadeer <haroon.qadeer@hotmail.com>
2026-06-02 08:33:03 +0500

    create profile -- personal info GUI

--- src/app/profile/create-profile/create-profile.component.ts
+++ src/app/profile/create-profile/create-profile.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-create-profile',
   templateUrl: './create-profile.component.html',
-  styleUrls: ['./create-profile.component.scss']
+  styleUrls: ['./create-profile.component.scss'],
 })
 export class CreateProfileComponent {
+  stepper: number = 1;
 
+  constructor() {}
+
+  ngOnInit() {
+    this.stepper = 1;
+  }
 }
